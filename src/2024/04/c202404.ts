ae9edfbd13b34a585b336eba59c2a8502292e08c
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-11-21 11:49:26 +0000

    04 done

--- src/2024/04/c202404.ts
+++ src/2024/04/c202404.ts
@@ -1,20 +1,23 @@
 import { Puzzle } from '../../lib/Puzzle';
 
-export class a202404 extends Puzzle {
+export class c202404 extends Puzzle {
     heights: number[];
+    median: number;
     minHeight: number;
     hits = 0;
 
     sampleMode(): void { };
 
     _loadData(lines: string[]) {
-        this.heights = this.lines.map(Number);
+        this.heights = this.lines.map(Number).sort();
+        this.median = this.heights[Math.floor(this.heights.length/2)];
         this.minHeight = this.heights.reduce((min, h) => Math.min(h, min), Infinity);
+        console.log(this.median);
     }
 
     _runStep(): boolean {
         let moreToDo = this.stepNumber < this.lines.length;
-        this.hits += this.heights[this.stepNumber-1] - this.minHeight;
+        this.hits += Math.abs(this.heights[this.stepNumber-1] - this.median);
         if (!moreToDo) {
             this.result = this.hits.toString();
         }
